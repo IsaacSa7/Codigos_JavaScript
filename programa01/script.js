6c3bc1340c93425e049fc58be3c4d8c14049da27
isaac <isaac.sa.silva08@aluno.ifce.edu.br>
2023-03-31 09:02:29 -0300

    subindo primeiro programa

---
+++ programa01/script.js
@@ -0,0 +1,8 @@
+const combobox = document.getElementById("combobox")
+const estados = ["Acre","Alagoas","Amapá","Amazonas","Bahia","Ceará","Distrito Federal","Espírito Santo","Goiás","Maranhão","Mato Grosso","Mato Grosso do Sul","Minas Gerais","Pará","Paraíba","Paraná","Pernambuco","Piauí","Rio de Janeiro","Rio Grande do Norte","Rio Grande do Sul","Rondônia","Roraima","Santa Catarina","São Paulo","Sergipe","Tocantins"]
+
+estados.map((el)=>{
+    const elemento = document.createElement("option")
+    elemento.innerHTML = el
+    combobox.appendChild(elemento)
+})
